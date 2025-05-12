39255cfb2461c3426153ea406f9aabb2ac10908e
ReactiveMatter <88178992+ReactiveMatter@users.noreply.github.com>
2025-05-12 22:23:42 +0530

    Update footer.php

--- template/include/footer.php
+++ template/include/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
 	<div class="container">
-	Made with ❤️ and <a href="https://reactivematter.github.io/tunnel-cms/">Tunnel CMS</a>.
+	Made with ❤️ and <a href="https://github.com/ReactiveMatter/tunnelcms">Tunnel CMS</a>.
 	</div>
 </footer>
